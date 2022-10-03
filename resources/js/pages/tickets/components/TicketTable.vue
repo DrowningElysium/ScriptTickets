6e35cda9e7ac8e5200f3def9d4c01c9377348ec8
Arjan Weurding <arjan@weurding.eu>
2022-10-03 14:08:36 +0200

    Sort tickets based on date and if the user is an admin

--- resources/js/pages/tickets/components/TicketTable.vue
+++ resources/js/pages/tickets/components/TicketTable.vue
@@ -1,9 +1,31 @@
 <script setup lang="ts">
+import { useAuthStore } from "@/stores";
 import type Ticket from "@/stores/ticket";
 import DateSpan from "@/components/DateSpan.vue";
 import CategoryDisplay from "@/pages/tickets/components/CategoryDisplay.vue";
+import { computed } from "vue";
 
 const props = defineProps<{ tickets: Ticket[] }>();
+const auth = useAuthStore();
+
+const tickets = computed(() => {
+    // Users want the most recently updated tickets found the fastest,
+    //      admins want the tickets that haven't had an update the longest.
+    return Object.values(props.tickets).sort((a: Ticket, b: Ticket) => {
+        const dateA = Date.parse(a.updated_at);
+        const dateB = Date.parse(b.updated_at);
+
+        if (dateA > dateB) {
+            return auth.user?.is_admin ? 1 : -1;
+        }
+
+        if (dateA < dateB) {
+            return auth.user?.is_admin ? -1 : 1;
+        }
+
+        return 0;
+    });
+});
 </script>
 
 <template>
@@ -20,7 +42,7 @@ const props = defineProps<{ tickets: Ticket[] }>();
             </tr>
         </thead>
         <tbody>
-            <tr v-for="ticket in props.tickets" :key="ticket.id">
+            <tr v-for="ticket in tickets" :key="ticket.id">
                 <td>{{ ticket.id }}</td>
                 <td>
                     <RouterLink
