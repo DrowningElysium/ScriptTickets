b3844a476c7e2b89161ae6007c5b94eb446e5041
Arjan Weurding <arjan@weurding.eu>
2022-10-03 14:08:13 +0200

    Sort Categories alphabetically

--- resources/js/pages/tickets/categories/components/CategoryTable.vue
+++ resources/js/pages/tickets/categories/components/CategoryTable.vue
@@ -1,8 +1,15 @@
 <script setup lang="ts">
 import type Category from "@/stores/category";
+import { computed } from "vue";
 import CategoryTableTitle from "./CategoryTableTitle.vue";
 
 const props = defineProps<{ categories: Category[] }>();
+const categories = computed(() => {
+    return Object.values(props.categories).sort((a: Category, b: Category) =>
+        // Titles are unique. So don't need to return 0.
+        a.title.toLowerCase() > b.title.toLowerCase() ? 1 : -1
+    );
+});
 </script>
 
 <template>
@@ -15,7 +22,7 @@ const props = defineProps<{ categories: Category[] }>();
             </tr>
         </thead>
         <tbody class="align-middle">
-            <tr v-for="category in props.categories" :key="category.id">
+            <tr v-for="category in categories" :key="category.id">
                 <td>{{ category.id }}</td>
                 <CategoryTableTitle :category="category" />
                 <td>{{ category.tickets_count }}</td>
